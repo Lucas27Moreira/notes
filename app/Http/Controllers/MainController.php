9ebdb9b8c60f791e40d7fc26296b87641417e813
Lucas Moreira <Lucas27Moreira>
2026-05-31 18:57:02 -0300

    added the notes to the home page

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -10,9 +11,16 @@ class MainController extends Controller
     public function index()
     {
         //load users notes
+        $id = session('user_id');
+        if (!$id) {
+            return redirect()->to('/login');
+        }
+
+        $user = User::find($id);
+        $notes = $user ? $user->notes()->get()->toArray() : [];
 
         //return home page
-        return view('home');
+        return view('home', ['notes' => $notes]);
     }
     public function newNote()
     {
